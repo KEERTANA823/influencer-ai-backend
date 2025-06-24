6c5ddc4811a7b4a5c0c41b76ccce647b37f5b45b
KEERTANA823 <keertanavijilakshmi@gmail.com>
2025-06-24 17:52:19 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,14 +1,12 @@
 import firebase_admin
-from firebase_admin import credentials, auth
-from fastapi import HTTPException
+from firebase_admin import credentials, auth as firebase_auth
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("firebase_credentials.json")
+cred = credentials.Certificate("FirebaseCredentials.json")
 firebase_admin.initialize_app(cred)
 
 def verify_token(token: str):
     try:
-        decoded_token = auth.verify_id_token(token)
+        decoded_token = firebase_auth.verify_id_token(token)
         return decoded_token
     except Exception as e:
-        raise HTTPException(status_code=401, detail="Invalid or expired token")
+        raise Exception("Invalid token") from e
